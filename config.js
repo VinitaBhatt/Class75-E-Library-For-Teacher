04121d3805b0589188e193cc4a001df1e13882e7
VinitaBhatt <71602249+VinitaBhatt@users.noreply.github.com>
2022-03-07 14:53:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6VpZOKLKLFod-c892irA9k3IyVSg_PeI",
+    authDomain: "e-library-f891e.firebaseapp.com",
+    projectId: "e-library-f891e",
+    storageBucket: "e-library-f891e.appspot.com",
+    messagingSenderId: "472710883221",
+    appId: "1:472710883221:web:e78afccebe643940a4389c"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  export default firebase.firestore();
